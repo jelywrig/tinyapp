b82a8eb0cf5b39a50c82a96f27544c25f0107f48
Jessie Wright <jessie.wright@gmail.com>
2020-04-13 22:21:08 +0000

    redirect after new url post to the url page for the newly created url

--- express_server.js
+++ express_server.js
@@ -16,8 +16,9 @@ app.get('/', (request, response) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/urls.json', (req, res) => {
@@ -29,7 +30,7 @@ app.get('/urls', (req,res) => {
   res.render('urls_index', templateVars);
 });
 app.get('/urls/new', (req, res) =>{
-  res.render("urls_new");
+  res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -49,12 +50,15 @@ app.listen(PORT, ()=>{
 
 //Helper functions
 
-function generateRandomString() {
-  return Math.random().toString(36).substr(2, 6);
+function generateRandomString(length) {
+  {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+    }
+    return result.join('');;
+ }
 }
 
-console.log(generateRandomString());
-console.log(generateRandomString());
-console.log(generateRandomString());
-console.log(generateRandomString());
-console.log(generateRandomString());
